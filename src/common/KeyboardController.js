775a8c994d9d862ac431ef718288a5d5c1394f22
lucasGabrielDeAA <lg.technus@gmail.com>
2020-08-22 00:08:35 -0300

    Common added

---
+++ src/common/KeyboardController.js
@@ -0,0 +1,66 @@
+import React, {useEffect, useCallback} from 'react';
+import {View, LayoutAnimation, Keyboard, Platform} from 'react-native';
+
+export default function KeyboardController({callback, children}) {
+  const onChangeKeyboard = useCallback(
+    (event) => {
+      if (!event) {
+        callback({keyboardHeight: 0});
+        return;
+      }
+
+      const {duration, easing, startCoordinates, endCoordinates} = event;
+
+      let keyboardHeight;
+
+      if (
+        Platform.OS === 'ios' &&
+        endCoordinates.screenY > startCoordinates.screenY
+      ) {
+        keyboardHeight = 0;
+      } else if (
+        Platform.OS === 'ios' &&
+        endCoordinates.screenY === startCoordinates.screenY
+      ) {
+        return;
+      } else {
+        keyboardHeight = endCoordinates.height;
+      }
+
+      let layoutAnimationConfig;
+
+      if (duration && easing) {
+        layoutAnimationConfig = {
+          duration,
+          update: {
+            duration,
+            type: LayoutAnimation.Types[easing] || 'keyboard',
+          },
+        };
+      }
+
+      callback({keyboardHeight, layoutAnimationConfig});
+    },
+    [callback],
+  );
+
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      Keyboard.addListener('keyboardWillChangeFrame', onChangeKeyboard);
+    } else {
+      Keyboard.addListener('keyboardDidShow', onChangeKeyboard);
+      Keyboard.addListener('keyboardDidHide', onChangeKeyboard);
+    }
+
+    return () => {
+      if (Platform.OS === 'ios') {
+        Keyboard.removeListener('keyboardWillChangeFrame');
+      } else {
+        Keyboard.removeListener('keyboardDidShow');
+        Keyboard.removeListener('keyboardDidHide');
+      }
+    };
+  }, []);
+
+  return <View>{children}</View>;
+}
