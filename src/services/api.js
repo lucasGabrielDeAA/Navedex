b47c4828714d9da47851dffbec5d642a7263ec01
lucasGabrielDeAA <lg.technus@gmail.com>
2020-08-22 00:09:49 -0300

    Services added

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+import {create} from 'axios';
+
+import config from '../config';
+
+const api = create({
+  baseURL: config.apiUrl,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+  responseType: 'json',
+  timeout: 30000,
+});
+
+export default api;
